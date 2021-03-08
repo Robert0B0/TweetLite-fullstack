fe748e48690859e8bff5e5a978f91f0ba66869da
Rob <robert.balog000@gmail.com>
2021-03-08 13:52:53 +0200

    - functional backend

---
+++ todo.md
@@ -0,0 +1,30 @@
+1. Tweets
+   -> User permissions
+   -> Creating
+      -> Text
+      -> Image
+   -> Delete
+   -> Retweet
+      -> Read only serializer
+      -> Create only serializer
+   -> Liking or Unliking
+
+2. Users
+   -> Register
+   -> Login
+   -> Logout
+   -> Profile
+   -> Image
+   -> Test
+   -> Follow Button
+   -> Feed
+   -> User' feed only?
+   -> Users + who they follow
+
+3. Following/ followers
+
+Long term todos
+
+- Notifications
+- Dm
+- Explore -> finding hastags
---
+++ tweets/tests.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from .models import Tweet
+
+User = get_user_model()
+
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='def', password='somepass')
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(content='my tweet', user=self.user)
+        self.assertEqual(tweet_obj.id, 1)
+        self.assertEqual(tweet_obj.user, self.user)
